67912622ffb6e16f8c231f78b4fa17aa7af3df20
eldadmozes <57889733+eldadmozes@users.noreply.github.com>
2023-03-14 20:14:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def hello_world():
     return f'Hello {sys.argv[1]}'
 if __name__ == '__main__':     
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=80)
 
