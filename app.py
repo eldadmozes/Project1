35c3c6df4ecda5ac167f852d8f3f23cdfb63b8a8
eldad mozes <eldad86.m@gmail.com>
2023-04-15 13:42:24 +0300

    pycharm

--- app.py
+++ app.py
@@ -61,18 +61,19 @@
 #
 # if __name__ == "__main__":
 #     app.run(debug=True)
-
+#
 import infra_flask
 import os.path
-from flask import Flask, request, redirect
+import subprocess
+from flask import Flask, request, redirect, url_for
 from flask.templating import render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, migrate
 import boto3
+import docker
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
 
 db = SQLAlchemy(app)
 
@@ -84,12 +85,11 @@ class Profile(db.Model):
     username = db.Column(db.String(20), unique=False, nullable=False)
     password = db.Column(db.String(20), unique=False, nullable=False)
 
-
     # def __str__(self):
     #     return f"Name:{self.first_name}, Age:{self.age}"
 
 
-@app.route("/signup", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def signup():
     # s3 = boto3.client('s3')
     # bucket_name = "sqlabs-devops-eldadm"
@@ -100,7 +100,7 @@ def signup():
         p = Profile(username=username, password=password)
         db.session.add(p)
         db.session.commit()
-        return f"hello {username}!"
+        return f"Hello {username}!" '<br> <a href="/homepage"><button>Next</button></a>'
 
     return render_template("signup.html")
 
@@ -108,7 +108,92 @@ def signup():
 @app.route("/homepage")
 def homepage():
     users_data = Profile.query.all()
-    return render_template("homepage.html", users_data=users_data)
+    return render_template("homepage.html")
+
+
+@app.route("/jenkins")
+def jenkins():
+    return render_template("jenkins.html")
+
+
+@app.route('/aws')
+def aws():
+    return render_template('aws.html')
+
+
+iam = boto3.client("iam")
+
+
+@app.route('/aws_create_iam', methods=['POST', 'GET'])
+def aws_create_iam():
+    if request.method == 'POST':
+        user_name = request.form.get('username')
+        password = request.form.get('password')
+        iam.create_user(UserName=user_name)
+        iam.add_user_to_group(GroupName='devops', UserName=user_name)
+        iam.create_login_profile(UserName=user_name, Password=password, PasswordResetRequired=False)
+        return f'Created successfully!'
+    return render_template("aws.html")
+
+
+ec2 = boto3.client("ec2")
+
+
+@app.route('/aws_create_ec2', methods=['POST', 'GET'])
+def aws_create_ec2():
+    if request.method == 'POST':
+        ec2_name = request.form.get('instance_name')
+        ec2_type = request.form.get('instance_type')
+        image_id = request.form.get('image_id')
+        install_docker = 'install_docker' in request.form
+        install_jenkins = request.form.get('install_jenkins')
+        install_flask = request.form.get('install_flask')
+
+        user_data = "#!/bin/bash\n"
+        if install_docker:
+            print(install_docker)
+            user_data += "sudo apt-get update && sudo apt -y install docker.io\n"
+
+            # log the user data to check if it's correct
+            # print(user_data)
+        # if install_jenkins:
+        #     user_data += 'sudo docker pull jenkins/jenkins:lts && sudo docker run -d -p 8080:8080 -p 50000:50000 --name jenkins -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts'
+        # if install_flask == 'yes':
+        #     user_data += 'sudo apt install python3-flask'
+        # security_group = request.form.get('security_group_id')
+        response = ec2.run_instances(
+            ImageId=image_id,
+            InstanceType=ec2_type,
+            MaxCount=1,
+            MinCount=1,
+            KeyName='jenkins-master',
+            UserData='user_data',
+            TagSpecifications=[{
+                'ResourceType': 'instance',
+                'Tags': [{
+                    'Key': 'Name',
+                    'Value': ec2_name
+                }]
+            }]
+        )
+        instance_id = response['Instances'][0]['InstanceId']
+
+        return f'Created successfully!'
+    return render_template("aws.html")
+
+
+@app.route("/docker", methods=["GET", "POST"])
+def docker():
+    if request.method == 'POST':
+        image_name = request.form.get('image_name')
+        subprocess.run(['docker', 'build', '-t', f'{image_name}', '.'])
+        subprocess.run(['docker', 'tag', f'{image_name}', f'eldad86/project1:{image_name}'])
+        subprocess.run(['docker', 'login', '-u', 'eldad86', '-p', 'Mivtzar!1961'])
+        subprocess.run(['docker', 'push', f'eldad86/project1:{image_name}'])
+        # subprocess.run(['docker', 'rmi', f'eldad86/project1:{image_name}'])
+        return f'Docker image {image_name} created and pushed to Docker Hub'
+    else:
+        return render_template('docker.html')
 
 
 if __name__ == "__main__":
--- infra_flask.py
+++ infra_flask.py
@@ -1,28 +1,16 @@
+import subprocess
+from flask import Flask, request, redirect, url_for
+from flask.templating import render_template
 import boto3
-import jenkins
 import docker
+iam = boto3.client("iam")
 
 
-def create_user():
-    iam = boto3.client("iam")
-    user_name = "my-user-test2"
+def create_iam_user(user_name, password):
     response = iam.create_user(UserName=user_name)
-    response = iam.create_login_profile(
-        UserName=user_name,
-        Password='Password@1234',
-        PasswordResetRequired=True
-    )
-    response = iam.add_user_to_group(
-        GroupName="devops",
-        UserName=user_name
-    )
-    response = iam.create_access_key(
-        UserName=user_name
-    )
+    iam.add_user_to_group(GroupName='devops', UserName=user_name)
+    iam.create_login_profile(UserName=user_name, Password=password, PasswordResetRequired=False)
+    # access_key = iam.create_access_key(UserName=user_name)
+    return response
 
 
-def show_user_info_in_web():
-    print(f"Full response: {response}")
-    print(f"Access Key ID: {response['AccessKey']['AccessKeyId']}")
-    print(f"Secret Access Key : {response['AccessKey']['SecretAccessKey']}")
-    print("Login link: https://626685713041.signin.aws.amazon.com/console")
