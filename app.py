5725e2ad7ccc1b1c27cc634fc289b637828dffb9
eldad mozes <eldad86.m@gmail.com>
2023-04-15 16:42:16 +0300

    pycharm

--- app.py
+++ app.py
@@ -1,67 +1,3 @@
-# import os.path
-# from flask import Flask, request, redirect
-# from flask.templating import render_template
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_migrate import Migrate, migrate
-# import boto3
-#
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-# app.config['UPLOAD_FOLDER'] = 'static/uploads/'
-# app.config['MAX_CONTENT'] = 16 * 1024 * 1024
-# ALLOWED_EXTENSIONS = ["png", "jpeg", "jpg", "gif"]
-#
-#
-# def allowed_file(filename):
-#     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-#
-#
-# db = SQLAlchemy(app)
-#
-# migrate = Migrate(app, db)
-#
-#
-# class Profile(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     first_name = db.Column(db.String(20), unique=False, nullable=False)
-#     last_name = db.Column(db.String(20), unique=False, nullable=False)
-#     age = db.Column(db.Integer, unique=False, nullable=False)
-#     picture = db.Column(db.String(100), unique=False, nullable=True)
-#
-#     def __str__(self):
-#         return f"Name: {self.first_name}, Age:{self.age}"
-#
-#
-# @app.route("/signup", methods=["GET", "POST"])
-# def signup():
-#     s3 = boto3.client('s3')
-#     bucket_name = "jenkins-sqlabs-eldadm"
-#     key_name = "download.png"
-#     if request.method == "POST":
-#         first_name = request.form.get("fname")
-#         last_name = request.form.get("lname")
-#         age = request.form.get("age")
-#         file = request.files.get("filename")
-#         if allowed_file(file.filename):
-#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-#             file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-#             s3.upload_file(file_path, bucket_name, key_name)
-#             p = Profile(first_name=first_name, last_name=last_name, age=age, picture=file.filename)
-#             db.session.add(p)
-#             db.session.commit()
-#         return f"{first_name} {last_name} {age}"
-#     return render_template("signup.html")
-#
-#
-# @app.route("/homepage")
-# def homepage():
-#     users_data = Profile.query.all()
-#     return render_template("homepage.html", users_data=users_data)
-#
-#
-# if __name__ == "__main__":
-#     app.run(debug=True)
-#
 import infra_flask
 import os.path
 import subprocess
@@ -71,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, migrate
 import boto3
 import docker
+import jenkins
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
@@ -112,7 +49,27 @@ def homepage():
 
 
 @app.route("/jenkins")
-def jenkins():
+def create_user_jenkins():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        fullname = request.form.get("fullname")
+        email = request.form.get("email")
+    # Connect to Jenkins server
+    server = jenkins.Jenkins('http://your-jenkins-server-url', username='your-jenkins-username',
+                             password='your-jenkins-api-token')
+
+    # Define the new user credentials
+    new_user = {
+        'username': 'new-user-username',
+        'password': 'new-user-password',
+        'fullName': 'New User Full Name',
+        'email': 'new.user@example.com'
+    }
+
+    # Create the new user
+    server.create_user(new_user['username'], new_user['password'], new_user['fullName'], new_user['email'])
+
     return render_template("jenkins.html")
 
 
@@ -156,8 +113,8 @@ def aws_create_ec2():
 
             # log the user data to check if it's correct
             # print(user_data)
-        # if install_jenkins:
-        #     user_data += 'sudo docker pull jenkins/jenkins:lts && sudo docker run -d -p 8080:8080 -p 50000:50000 --name jenkins -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts'
+        if install_jenkins:
+            user_data += 'sudo docker pull jenkins/jenkins:lts && sudo docker run -d -p 8080:8080 -p 50000:50000 --name jenkins -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts'
         # if install_flask == 'yes':
         #     user_data += 'sudo apt install python3-flask'
         # security_group = request.form.get('security_group_id')
@@ -167,7 +124,7 @@ def aws_create_ec2():
             MaxCount=1,
             MinCount=1,
             KeyName='jenkins-master',
-            UserData='user_data',
+            UserData=user_data,
             TagSpecifications=[{
                 'ResourceType': 'instance',
                 'Tags': [{
